6717b89bce15632f0fe5c794fcd6054376682dfe
root <root@DESKTOP-F0J8EN2.localdomain>
2018-09-27 01:55:18 -0700

    List View finally working

--- mysite/myblog/urls.py
+++ mysite/myblog/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from myblog.views import stub_view, list_view
 
 urlpatterns = [
-    url(r'^$', stub_view, name="blog_index"),
+    url(r'^$', list_view, name="blog_index"),
     url(r'^posts/(\d+)/$', list_view, name="blog_detail"),
 ]
